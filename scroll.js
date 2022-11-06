98c0c079155d77e5ded1fc1546323d78deca3c2a
gabrielprogramadorads <gabriel.programador.ads@gmail.com>
2022-11-06 00:01:52 -0300

    att

--- scroll.js
+++ scroll.js
@@ -3,10 +3,7 @@ window.sr = ScrollReveal({ reset: true});
 sr.reveal('.text-bx', { duration: 1000});
 
 sr.reveal('.img-avatar', {  duration: 2000});
-
-
-
-
+sr.reveal('.img-bx', {  duration: 2500});
 
 sr.reveal('.resumo', { 
     rotate: { x: 0, y: 80, z: 0},
